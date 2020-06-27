f00e3c38c73144f2989bc71ceef34e2e23108348
Vegetto <git@angelnu.com>
2020-06-27 13:03:07 +0200

    Fix setOptionBytes

--- tools/init_lorawan_keys.py
+++ tools/init_lorawan_keys.py
@@ -195,9 +195,9 @@ class lorawan_device:
 
         for line in programmer_output.split('\n'):
             if "Flash size" in line:
-                if "64 KBytes":
+                if "64 KBytes" in line:
                     self.__flash_size = 64 * 1024
-                elif "128 KBytes":
+                elif "128 KBytes" in line:
                     self.__flash_size = 128 * 1024
                 else:
                     raise NameError("Unrecognised flash size")
@@ -237,14 +237,16 @@ class lorawan_device:
         if (len(write_optionbytes) == 0):
             return #Nothing to change
         
-        WRITE_OPTIONBYTES_CMD = PROGRAMMER + " " + write_optionbytes
-        print(WRITE_OPTIONBYTES_CMD)
-        try:
-            read_optionbytes_output = subprocess.check_output(WRITE_OPTIONBYTES_CMD.split(" "),universal_newlines=True)
-        except subprocess.CalledProcessError as e:
-            print("ERROR5: \n"+e.output)
+        for i in range(2):
+            WRITE_OPTIONBYTES_CMD = PROGRAMMER + write_optionbytes
+            print(WRITE_OPTIONBYTES_CMD)
+            try:
+                read_optionbytes_output = subprocess.check_output(WRITE_OPTIONBYTES_CMD.split(" "),universal_newlines=True)
+                break
+            except subprocess.CalledProcessError as e:
+                print("ERROR5: \n"+e.output)
+        else:
             sys.exit(-1)
-
         
         
         
