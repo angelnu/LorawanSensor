00add90960cebf4d6d7c1ef21f641fb7a6595233
Vegetto <git@angelnu.com>
2020-08-31 03:09:05 +0200

    do no pull dow SDO

--- src/sensors/accelerometer_LIS3DH.cpp
+++ src/sensors/accelerometer_LIS3DH.cpp
@@ -66,6 +66,10 @@ static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp, uint16_t 
 void Sensor_accelerometer::init(bool firstTime){
     
     if (firstTime) {
+        
+		//Pull-up to select I2c mode
+        pinMode(PIN_ACCEL_SDO, INPUT_PULLUP);
+        
         /*
         *  Initialize mems driver interface
         */
